6976405208cb2d7a6e40ab94f48fe588cf7679f2
Matheus Pereira Dias <matheuspd07@gmail.com>
2024-05-14 19:46:30 -0300

    bcmd done

--- bcmd/main.c
+++ bcmd/main.c
@@ -10,34 +10,34 @@
  */
 
 #include "bios.h"
-#include "utils.h"
+#include "opt.h"
 
-#define PROMPT "$ "		/* Prompt sign.      */
-#define SIZE 20			/* Read buffer size. */
+#define PROMPT "$ "		/* Prompt sign. */
+#define SIZE 20	/* Read buffer size. */
 
-char buffer[SIZE];		/* Read buffer.      */
+char buffer[SIZE];		/* Read buffer. */
+int len = 0;
 
-int main()
-{
-  clear();
-  
-  println  ("Boot Command 1.0");
+int main() {
+  clear();  
+  println("Boot Cmd 1.0");
 
-  while (1)
-    {
-      print(PROMPT);		/* Show prompt.               */
-      readln(buffer);		/* Read use input.            */
-
-      if (buffer[0])		/* Execute built-in command.  */
-	{
-	  if (!strcmp(buffer,"help"))
-	    println("A Beattles's song.");
-	  else 
-	    println("Unkown command.");
-	}
-    }
-  
-  return 0;
-
+  while (1) {    	
+		print(PROMPT);		/* Show prompt. */
+    	readln(buffer);		/* Read use input. */
+    	if (buffer[0]) {		/* Execute built-in command. */
+		    if (!strcmp(buffer,"strlen")) {				
+		      	readln(buffer);
+				print("Str: ");
+				println(buffer);
+				len = strlen(buffer);
+				itoa(len, buffer);
+				print("Len: ");
+				println(buffer);
+    	    }
+		    else println("Not found.");
+		}
+  	}  
+  	return 0;
 }
 
