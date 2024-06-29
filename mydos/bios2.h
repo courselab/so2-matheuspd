635caafd1cac4106e816b2c3714edbbaa0e9d741
Matheus Pereira Dias <matheuspd07@gmail.com>
2024-06-29 00:16:42 -0300

    Add files

---
+++ mydos/bios2.h
@@ -0,0 +1,19 @@
+/*
+ *    SPDX-FileCopyrightText: 2024 Matheus Pereira Dias <matheuspd07@gmail.com>
+ *    SPDX-FileCopyrightText: 2024 Monaco F. J. <monaco@usp.br>
+ *   
+ *    SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *  This file is a derivative work from SYSeg (https://gitlab.com/monaco/syseg)
+ *  and contains modifications carried out by the following author(s):
+ *  Matheus Pereira Dias <matheuspd07@gmail.com>
+ */
+
+#ifndef BIOS2_H
+#define BIOS2_H
+
+int __attribute__((fastcall)) kread(char *);
+
+void __attribute__((fastcall)) udelay(unsigned short);
+
+#endif  /* BIOS2_H  */
